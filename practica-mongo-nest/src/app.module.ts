c1c275b74eaf0a6316f2ba3ad32a4253aa1cbe7f
thiago <thiagozambonini24@gmail.com>
2023-09-19 11:15:53 -0300

    DB connection and enviroment variables

--- practica-mongo-nest/src/app.module.ts
+++ practica-mongo-nest/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeriesModule } from './modules/series/series.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [SeriesModule],
+  imports: [SeriesModule, MongooseModule.forRoot(process.env.MONGO)],
 })
 export class AppModule {}
