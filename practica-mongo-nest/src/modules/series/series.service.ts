d75d89e68a4b38828cf5dfe4ea3488f4a49be4ea
thiago <thiagozambonini24@gmail.com>
2023-09-20 09:16:16 -0300

    Using schema in service

--- practica-mongo-nest/src/modules/series/series.service.ts
+++ practica-mongo-nest/src/modules/series/series.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Serie } from '../../schemas/serie.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class SeriesService {}
+export class SeriesService {
+  constructor(@InjectModel(Serie.name) private taskModel: Model<Serie>) {}
+}
