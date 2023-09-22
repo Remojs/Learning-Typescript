9039c38ba704bdb68b039a7729dc0cd72d6b1700
thiago <thiagozambonini24@gmail.com>
2023-09-22 18:17:15 -0300

    Controllers

--- Proyecto3-MongoCRUD/src/modules/series/series.controller.ts
+++ Proyecto3-MongoCRUD/src/modules/series/series.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { SeriesService } from './series.service';
+import { CreateSerieDTO } from 'src/dto/create-serie.dto';
 
 @Controller('series')
-export class SeriesController {}
+export class SeriesController {
+  constructor(private seriesService: SeriesService) {}
+
+  @Get()
+  findAll() {
+    return this.seriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.seriesService.findOne(id);
+  }
+
+  @Post()
+  createOne(@Body() body: CreateSerieDTO) {
+    return this.seriesService.create(body);
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return this.seriesService.delete(id);
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() body: any) {
+    return this.seriesService.update(id, body);
+  }
+}
