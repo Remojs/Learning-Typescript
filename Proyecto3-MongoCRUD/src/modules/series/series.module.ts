2b93f8b7237e052e16bf69816bbd9a28137fd2b6
thiago <thiagozambonini24@gmail.com>
2023-09-22 16:39:04 -0300

    Series module connected with DB

--- Proyecto3-MongoCRUD/src/modules/series/series.module.ts
+++ Proyecto3-MongoCRUD/src/modules/series/series.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeriesController } from './series.controller';
 import { SeriesService } from './series.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SerieSchema, Serie } from 'src/schemas/serie.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Serie.name, schema: SerieSchema }]),
+  ],
   controllers: [SeriesController],
-  providers: [SeriesService]
+  providers: [SeriesService],
 })
 export class SeriesModule {}
