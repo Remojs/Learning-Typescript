55e9e3aef8693cd0bbd550c3c4ee5eb5745f7468
thiago <thiagozambonini24@gmail.com>
2023-09-22 10:37:46 -0300

    Serie.service functions

--- practica-mongo-nest/src/modules/series/series.service.ts
+++ practica-mongo-nest/src/modules/series/series.service.ts
@@ -5,5 +5,27 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class SeriesService {
-  constructor(@InjectModel(Serie.name) private taskModel: Model<Serie>) {}
+  constructor(@InjectModel(Serie.name) private serieModel: Model<Serie>) {}
+
+  findAll() {
+    this.serieModel.find();
+  }
+
+  async create(createdSerie: any) {
+    const newSerie = new this.serieModel(createdSerie);
+    await newSerie.save();
+    return newSerie;
+  }
+
+  async findOne(id: string) {
+    return this.serieModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.serieModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, serie: any) {
+    return this.serieModel.findByIdAndUpdate(id, serie);
+  }
 }
