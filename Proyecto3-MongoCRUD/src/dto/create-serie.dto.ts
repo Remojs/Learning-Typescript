089e997971bbfe553895eef3fc87a1199b169724
thiago <thiagozambonini24@gmail.com>
2023-09-22 16:07:21 -0300

    DTO Validations

--- Proyecto3-MongoCRUD/src/dto/create-serie.dto.ts
+++ Proyecto3-MongoCRUD/src/dto/create-serie.dto.ts
@@ -1,6 +1,24 @@
-export interface CreateSerieDTO {
+import {
+  IsString,
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class CreateSerieDTO {
+  @IsString()
+  @IsNotEmpty()
   title: string;
-  description: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
   episodes: number;
+
+  @IsBoolean()
+  @IsOptional()
   finished?: boolean;
 }
--- Proyecto3-MongoCRUD/src/dto/update-serie.dto.ts
+++ Proyecto3-MongoCRUD/src/dto/update-serie.dto.ts
@@ -1,6 +1,19 @@
-export interface UpdateSerieDTO {
-  title?: string;
+import { IsString, IsBoolean, IsNumber, IsOptional } from 'class-validator';
+
+export class UpdateSerieDTO {
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
-  episodes?: number;
+
+  @IsNumber()
+  @IsOptional()
+  episodes: number;
+
+  @IsBoolean()
+  @IsOptional()
   finished?: boolean;
 }
