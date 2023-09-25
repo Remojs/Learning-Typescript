6d152cf479d3b89ede650ec872143da0239cb332
thiago <thiagozambonini24@gmail.com>
2023-09-25 17:33:11 -0300

    TypeORM config

--- Proyecto4-TypeORM-MySQL/src/app.module.ts
+++ Proyecto4-TypeORM-MySQL/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './modules/users/controllers/user.controller';
+import { UsersController } from './modules/users/controller/user.controller';
 import { UsersService } from './modules/users/services/user.service';
 import { UsersModule } from './modules/users/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'NestP4-DB',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [UsersModule],
 })
 export class AppModule {}
