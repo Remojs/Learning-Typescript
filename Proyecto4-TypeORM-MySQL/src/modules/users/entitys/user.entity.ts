af77a7e849ba7bf5340ab206d8dd4fa62d085e1f
thiago <thiagozambonini24@gmail.com>
2023-09-28 10:06:38 -0300

    table relations

--- Proyecto4-TypeORM-MySQL/src/modules/users/entitys/user.entity.ts
+++ Proyecto4-TypeORM-MySQL/src/modules/users/entitys/user.entity.ts
@@ -1,19 +1,30 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+import { Profile } from "./profile.entity";
 
-@Entity({ name: 'users'})
+@Entity({ name: "users" })
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({unique: true})
+  @Column({ unique: true })
   username: string;
 
   @Column()
   password: string;
 
-  @Column({ type: 'datetime',default: () => 'CURRENT_TIMESTAMP'})
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
   createdAt: Date;
 
-  @Column({nullable: true})
+  @Column({ nullable: true })
   auth: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
 }
