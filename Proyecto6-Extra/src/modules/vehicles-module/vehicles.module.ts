3f64e677a52eb4001f3b2d89ba1e35d86487a60c
thiago <thiagozambonini24@gmail.com>
2023-10-16 16:12:07 -0300

    Project 6 - Finished

--- Proyecto6-Extra/src/modules/vehicles-module/vehicles.module.ts
+++ Proyecto6-Extra/src/modules/vehicles-module/vehicles.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Vehicle, VehicleSchema } from 'src/schemas/vehicle.schema';
+import { GetByModelService } from './services/getByModel.service';
+import { GetByModelController } from './controllers/getByModel.controller';
+import { GetByBrandService } from './services/getByBrand.service';
+import { GetByBrandController } from './controllers/getByBrand.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [GetByModelController, GetByBrandController],
+  providers: [GetByModelService, GetByBrandService],
 })
 export class VehicleModule {}
